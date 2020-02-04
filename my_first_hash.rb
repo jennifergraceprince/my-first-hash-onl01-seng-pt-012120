580633142c09d0499350cd5235e7c1caa247c786
Jennifer Prince <jennifergraceprince@gmail.com>
2020-02-04 01:07:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,7 +24,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  the_manifest["oil paintings"]
+  shipping_manifest["oil paintings"]
 
 end
 
@@ -35,11 +35,11 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-the_manifest["muskets"] = "2"
+shipping_manifest["muskets"] = "2"
 
 
   # add 4 gun powder to the shipping_manifest hash below
-the_manifest["gun powder"] = "4"
+shipping_manifest["gun powder"] = "4"
 
 
   # return the shipping_manifest hash below
